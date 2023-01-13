6a5d92826d6bc92cb9d2fee97de02232e856e045
Ramon Queiroz dos Santos <ramonqueirozs@gmail.com>
2023-01-13 14:29:34 -0300

    Fix: Inclui um navigation controller

--- AppAlcoolXGasolina/AppAlcoolXGasolina/SceneDelegate.swift
+++ AppAlcoolXGasolina/AppAlcoolXGasolina/SceneDelegate.swift
@@ -14,8 +14,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 	func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
 		guard let windowScene = (scene as? UIWindowScene) else { return }
 		window = UIWindow(windowScene: windowScene)
-		window?.frame = UIScreen.main.bounds
-		window?.rootViewController = HomeVC()
+		let nav = UINavigationController(rootViewController: HomeVC())
+		window?.rootViewController = nav
 		window?.makeKeyAndVisible()
 	}
 
