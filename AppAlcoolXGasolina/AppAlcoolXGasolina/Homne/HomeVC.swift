a480758d8d6d91611e201ddb5c4a9d9bd397df3d
Ramon Queiroz dos Santos <ramonqueirozs@gmail.com>
2023-01-13 11:16:42 -0300

    Feat: Cria e configura a tela inicial

--- AppAlcoolXGasolina/AppAlcoolXGasolina/Homne/HomeVC.swift
+++ AppAlcoolXGasolina/AppAlcoolXGasolina/Homne/HomeVC.swift
@@ -8,10 +8,16 @@
 import UIKit
 
 class HomeVC: UIViewController {
+	
+	var screen: HomeScreen?
+	
+	override func loadView() {
+		screen = HomeScreen()
+		view = screen
+	}
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		view.backgroundColor = .blue
 	}
 
 
--- AppAlcoolXGasolina/AppAlcoolXGasolina/Homne/Screen/HomeScreen.swift
+++ AppAlcoolXGasolina/AppAlcoolXGasolina/Homne/Screen/HomeScreen.swift
@@ -8,13 +8,36 @@
 import UIKit
 
 class HomeScreen: UIView {
+	
+	lazy var backgroundImageView: UIImageView = {
+		let img = UIImageView()
+		img.translatesAutoresizingMaskIntoConstraints = false
+		img.image = UIImage(named: "BG HOME")
+		img.contentMode = .scaleAspectFit
+		return img
+	}()
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		self.configSuperView()
+		self.setupConstraints()
+	}
+	
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
+	private func configSuperView(){
+		self.addSubview(self.backgroundImageView)
+	}
+	
+	private func setupConstraints(){
+		NSLayoutConstraint.activate([
+			self.backgroundImageView.topAnchor.constraint(equalTo: self.topAnchor),
+			self.backgroundImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+			self.backgroundImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+			self.backgroundImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+		])
+	}
 
 }
