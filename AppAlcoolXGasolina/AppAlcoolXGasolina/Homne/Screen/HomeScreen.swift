9a14e00db3a3d4c299b049459f474a3fd1974ad2
Ramon Queiroz dos Santos <ramonqueirozs@gmail.com>
2023-01-13 11:15:00 -0300

    Feat: Add Images do projeto

---
+++ AppAlcoolXGasolina/AppAlcoolXGasolina/Homne/Screen/HomeScreen.swift
@@ -0,0 +1,20 @@
+//
+//  HomeScreen.swift
+//  AppAlcoolXGasolina
+//
+//  Created by Ramon Queiroz dos Santos on 13/01/23.
+//
+
+import UIKit
+
+class HomeScreen: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
